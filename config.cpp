9f540557027b8a0e679dcc7de64f500d5c842057
iskinmike <Myasnikov.mike@gmail.com>
2015-07-31 18:41:30 +0500

    Код отформатирован в clang

--- config.cpp
+++ config.cpp
@@ -1,27 +1,26 @@
 #ifdef _WIN32
-	#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 #endif
 
 #include <string>
 #include <windows.h>
 EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 
-
 std::string getConfigPath() {
-    std::string result = "";
-    WCHAR DllPath[MAX_PATH] = { 0 };
+  std::string result = "";
+  WCHAR DllPath[MAX_PATH] = {0};
 
-    GetModuleFileNameW((HINSTANCE)&__ImageBase, DllPath, (DWORD)MAX_PATH);
+  GetModuleFileNameW((HINSTANCE)&__ImageBase, DllPath, (DWORD)MAX_PATH);
 
-    WCHAR *tmp = wcsrchr(DllPath, L'\\');
-    WCHAR wConfigPath[MAX_PATH] = { 0 };
-    size_t path_len = tmp - DllPath;
-    wcsncpy(wConfigPath, DllPath, path_len);
-    wcscat(wConfigPath, L"\\config.ini");
+  WCHAR *tmp = wcsrchr(DllPath, L'\\');
+  WCHAR wConfigPath[MAX_PATH] = {0};
+  size_t path_len = tmp - DllPath;
+  wcsncpy(wConfigPath, DllPath, path_len);
+  wcscat(wConfigPath, L"\\config.ini");
 
-    char ConfigPath[MAX_PATH] = { 0 };
-    wcstombs(ConfigPath, wConfigPath, sizeof(ConfigPath));
+  char ConfigPath[MAX_PATH] = {0};
+  wcstombs(ConfigPath, wConfigPath, sizeof(ConfigPath));
 
-    result.append(ConfigPath);
-    return result;
+  result.append(ConfigPath);
+  return result;
 }
--- u3d_function_module.h
+++ u3d_function_module.h
@@ -5,81 +5,78 @@
 */
 
 #ifndef U3D_FUNCTION_MODULE_H
-#define	U3D_FUNCTION_MODULE_H
+#define U3D_FUNCTION_MODULE_H
 
 struct CondBoolString;
 struct MutexAndBoxVector;
-struct SocketAndBuffer{
-	boost::asio::ip::tcp::socket *socket_;
-	char *buffer_;
+struct SocketAndBuffer {
+  boost::asio::ip::tcp::socket *socket_;
+  char *buffer_;
 };
 
 class u3dFunctionModule : public FunctionModule {
-	FunctionData **u3d_functions;
-	colorPrintfModuleVA_t *colorPrintf_p;
+  FunctionData **u3d_functions;
+  colorPrintfModuleVA_t *colorPrintf_p;
 
-	bool is_world_initialized;
-	int postmans_uniq_id;
+  bool is_world_initialized;
+  int postmans_uniq_id;
 
-	std::vector<int> ids_of_created_objects;
+  std::vector<int> ids_of_created_objects;
 
-	void modulePostmanThread();
-	void moduleRecieverThread();
-	void read_handler(
-		SocketAndBuffer *sock_and_buff_struct,
-		const boost::system::error_code& ec,
-		std::size_t bytes_transferred
-	);
-	void write_handler(
-		const boost::system::error_code& ec,
-		std::size_t bytes_transferred
-	);
-	void connect_handler(const boost::system::error_code& ec);
+  void modulePostmanThread();
+  void moduleRecieverThread();
+  void read_handler(SocketAndBuffer *sock_and_buff_struct,
+                    const boost::system::error_code &ec,
+                    std::size_t bytes_transferred);
+  void write_handler(const boost::system::error_code &ec,
+                     std::size_t bytes_transferred);
+  void connect_handler(const boost::system::error_code &ec);
 
-	boost::mutex postman_exit_mutex;
-	bool postman_thread_exit;
+  boost::mutex postman_exit_mutex;
+  bool postman_thread_exit;
 
-	boost::asio::io_service robot_io_service_;
-	boost::asio::ip::tcp::socket module_socket;
+  boost::asio::io_service robot_io_service_;
+  boost::asio::ip::tcp::socket module_socket;
 
-	// Postman thread variables
-	boost::thread *module_postman_thread;
-	boost::mutex postman_thread_mutex;
-	bool postman_thread_waker_flag;
-	boost::condition_variable postman_thread_waker;
+  // Postman thread variables
+  boost::thread *module_postman_thread;
+  boost::mutex postman_thread_mutex;
+  bool postman_thread_waker_flag;
+  boost::condition_variable postman_thread_waker;
 
-	boost::thread *module_reciever_thread;
-	boost::mutex module_mutex;
+  boost::thread *module_reciever_thread;
+  boost::mutex module_mutex;
 
-	MutexAndBoxVector *data_for_shared_memory;
-	SocketAndBuffer *sock_and_buff_struct;
-	std::vector<CondBoolString *> *box_of_messages;
-	std::map<int, CondBoolString *> postmans_map_of_mailed_messages;
+  MutexAndBoxVector *data_for_shared_memory;
+  SocketAndBuffer *sock_and_buff_struct;
+  std::vector<CondBoolString *> *box_of_messages;
+  std::map<int, CondBoolString *> postmans_map_of_mailed_messages;
 
-public:
-	u3dFunctionModule();
+ public:
+  u3dFunctionModule();
 
-	//init
-	const char *getUID();
-	void prepare(colorPrintfModule_t *colorPrintf_p, colorPrintfModuleVA_t *colorPrintfVA_p);
+  // init
+  const char *getUID();
+  void prepare(colorPrintfModule_t *colorPrintf_p,
+               colorPrintfModuleVA_t *colorPrintfVA_p);
 
-	//compiler only
-	FunctionData **getFunctions(unsigned int *count_functions);
-	void *writePC(unsigned int *buffer_length);
+  // compiler only
+  FunctionData **getFunctions(unsigned int *count_functions);
+  void *writePC(unsigned int *buffer_length);
 
-	//intepreter - program & lib
-	void readPC(void *buffer, unsigned int buffer_length);
+  // intepreter - program & lib
+  void readPC(void *buffer, unsigned int buffer_length);
 
-	//intepreter - program
-	FunctionResult* executeFunction(system_value function_index, void **args);
-	int startProgram(int uniq_index);
-	int endProgram(int uniq_index);
+  // intepreter - program
+  FunctionResult *executeFunction(system_value function_index, void **args);
+  int startProgram(int uniq_index);
+  int endProgram(int uniq_index);
 
-	//destructor
-	void destroy();
-	~u3dFunctionModule() {};
+  // destructor
+  void destroy();
+  ~u3dFunctionModule(){};
 
-	void colorPrintf(ConsoleColor colors, const char *mask, ...);
+  void colorPrintf(ConsoleColor colors, const char *mask, ...);
 };
 
-#endif	/* U3D_FUNCTION_MODULE_H  */
+#endif /* U3D_FUNCTION_MODULE_H  */
